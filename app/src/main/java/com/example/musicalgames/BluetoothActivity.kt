b3d09d6c822198c43fb93f9f51e757958689077b
Izabela Tylek <izabela.tylek@student.uj.edu.pl>
2024-05-02 14:18:09 +0200

    Add 'Make discoverable' button

--- app/src/main/java/com/example/musicalgames/BluetoothActivity.kt
+++ app/src/main/java/com/example/musicalgames/BluetoothActivity.kt
@@ -1,19 +1,27 @@
 package com.example.musicalgames
 import android.Manifest
 import android.bluetooth.BluetoothAdapter
+import android.bluetooth.BluetoothDevice
 import android.bluetooth.BluetoothManager
+import android.content.BroadcastReceiver
+import android.content.Context
 import android.content.Intent
+import android.content.IntentFilter
+import android.content.pm.PackageManager
 import android.os.Build
+import android.os.Build.VERSION
 import android.os.Bundle
 import android.util.Log
 import android.widget.Button
 import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 
 class BluetoothActivity : AppCompatActivity() {
 
     private lateinit var buttonEnableBluetooth: Button
+    private lateinit var buttonMakeDiscoverable: Button
     private lateinit var bluetoothAdapter: BluetoothAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -27,6 +35,12 @@ class BluetoothActivity : AppCompatActivity() {
         buttonEnableBluetooth = findViewById(R.id.button_enable_bluetooth)
         buttonEnableBluetooth.setOnClickListener {
             enableBluetooth()
+            discoverDevices()
+        }
+        buttonMakeDiscoverable=findViewById(R.id.button_make_discoverable)
+        buttonMakeDiscoverable.setOnClickListener{
+            enableBluetooth()
+            makeDiscoverable()
         }
     }
 
@@ -34,13 +48,86 @@ class BluetoothActivity : AppCompatActivity() {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
             requestMultiplePermissions.launch(arrayOf(
                 Manifest.permission.BLUETOOTH_SCAN,
-                Manifest.permission.BLUETOOTH_CONNECT))
+                Manifest.permission.BLUETOOTH_CONNECT,
+                Manifest.permission.BLUETOOTH_ADMIN))
         }
         else{
             val enableBtIntent = Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE)
             requestBluetooth.launch(enableBtIntent)
         }
     }
+    private fun discoverDevices() {
+        val permissions = arrayOf(
+            Manifest.permission.ACCESS_FINE_LOCATION,
+            Manifest.permission.ACCESS_COARSE_LOCATION
+        )
+        requestMultiplePermissions.launch(permissions)
+
+        if((ContextCompat.checkSelfPermission(
+                this, Manifest.permission.ACCESS_FINE_LOCATION,)
+            != PackageManager.PERMISSION_GRANTED)
+            ||(ContextCompat.checkSelfPermission(
+                this, Manifest.permission.ACCESS_COARSE_LOCATION,)
+                    != PackageManager.PERMISSION_GRANTED)
+            ){
+            //return or sth
+        }
+
+        // Register BroadcastReceiver for Bluetooth discovery events
+        val filter = IntentFilter().apply {
+            addAction(BluetoothDevice.ACTION_FOUND)
+            addAction(BluetoothDevice.ACTION_PAIRING_REQUEST)
+        }
+        registerReceiver(bluetoothReceiver, filter)
+
+        // Start Bluetooth device discovery
+        bluetoothAdapter.startDiscovery()
+    }
+    private fun toast(text: String) {
+        Toast.makeText(this, text, Toast.LENGTH_SHORT)
+    }
+
+    // BroadcastReceiver for Bluetooth events
+    private val bluetoothReceiver = object : BroadcastReceiver() {
+        override fun onReceive(context: Context?, intent: Intent?) {
+            when (intent?.action) {
+                BluetoothDevice.ACTION_FOUND -> {
+                    // Handle discovered devices
+                    val device: BluetoothDevice? =
+                        if(VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+                        intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE, BluetoothDevice::class.java)}
+                        else {intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE)}
+                    toast("found a device")
+                    // Handle the discovered device
+
+                }
+                BluetoothDevice.ACTION_PAIRING_REQUEST -> {
+                    // Handle pairing request
+                    val device: BluetoothDevice? =
+                        if(VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+                            intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE, BluetoothDevice::class.java)}
+                        else {intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE)}
+
+                    val pin: String? = intent.getStringExtra(BluetoothDevice.EXTRA_PAIRING_KEY)
+                    // Handle the pairing request
+                }
+            }
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        unregisterReceiver(bluetoothReceiver)
+    }
+
+    private fun makeDiscoverable() {
+        //TODO: bluetooth admin permission - currently in enableBluetooth
+        val discoverableIntent = Intent(BluetoothAdapter.ACTION_REQUEST_DISCOVERABLE).apply {
+            putExtra(BluetoothAdapter.EXTRA_DISCOVERABLE_DURATION, 60)
+        }
+        startActivity(discoverableIntent)
+    }
+
 
     private var requestBluetooth =
         registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
---
+++ app/src/main/java/com/example/musicalgames/bluetooth/BluetoothServerController.kt
@@ -0,0 +1,12 @@
+package com.example.musicalgames.bluetooth
+
+import android.bluetooth.BluetoothAdapter
+import android.bluetooth.BluetoothServerSocket
+import android.bluetooth.BluetoothSocket
+import com.example.musicalgames.MainActivity
+import java.io.IOException
+import java.util.UUID
+
+class BluetoothServerController {
+
+}
