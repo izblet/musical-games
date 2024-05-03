53b04efde26e579819faa898d602ffe545b9e89c
Izabela Tylek <izabela.tylek@student.uj.edu.pl>
2024-05-03 22:26:47 +0200

    Fix tensorflow dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -51,5 +51,5 @@ dependencies {
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
     implementation("org.tensorflow:tensorflow-lite:0.0.0-nightly-SNAPSHOT")
-    //implementation("org.tensorflow:tensorflow-lite-select-tf-ops:0.0.0.nightly-SNAPSHOT")
+    implementation("org.tensorflow:tensorflow-lite-select-tf-ops:0.0.0-nightly-SNAPSHOT")
 }
