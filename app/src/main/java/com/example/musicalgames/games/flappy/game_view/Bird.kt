59d764e41de8ef12d2efe2a6e162d997a4a99b8b
Izabela Tylek <izabela.tylek@student.uj.edu.pl>
2024-06-16 19:33:49 +0200

    Bird is an ellipse for collisions

--- app/src/main/java/com/example/musicalgames/games/flappy/game_view/Bird.kt
+++ app/src/main/java/com/example/musicalgames/games/flappy/game_view/Bird.kt
@@ -26,6 +26,12 @@ class Bird(private val pitchRecogniser: PitchRecogniser, private val minPitch: D
     fun passing(pipe: Pipe) : Boolean {
         return x > pipe.x + Pipe.WIDTH && x < pipe.x + Pipe.WIDTH + Pipe.SPEED
     }
+
+    fun pointInsideEllipse(x: Float, y: Float, cx: Float, cy: Float, a: Float, b: Float): Boolean {
+        val dx = x - cx
+        val dy = y - cy
+        return (dx * dx) / (a * a) + (dy * dy) / (b * b) <= 1
+    }
     fun intersects(pipe: Pipe, width:Float, height:Float):Boolean {
         // the radius is vertical radius, when displayed on the screen it has to be scaled
         val horizontalRadius = radius*(width/height)
@@ -37,8 +43,45 @@ class Bird(private val pitchRecogniser: PitchRecogniser, private val minPitch: D
         )
         val topPipeRect = pipe.getTopRect()
         val bottomPipeRect = pipe.getBottomRect()
+        //return birdRect.intersect(topPipeRect) || birdRect.intersect(bottomPipeRect)
 
-        return birdRect.intersect(topPipeRect) || birdRect.intersect(bottomPipeRect)
+        if(birdRect.intersect(topPipeRect)) {
+            if(y<topPipeRect.left) {
+                //the ellipse would have to intersect the left corner
+                if(pointInsideEllipse(topPipeRect.left, topPipeRect.bottom, x, y, horizontalRadius, radius))
+                    return true
+
+            } else if(topPipeRect.right<y) {
+                //the ellipse would have to intersect the right corner
+                if(pointInsideEllipse(topPipeRect.right, topPipeRect.bottom, x, y, horizontalRadius, radius))
+                    return true
+            }
+            else {
+                //the middle part of the ellipse is between the left and right side of the pipe
+                //it has to intersect
+                return true
+            }
+
+        }
+        if(birdRect.intersect(bottomPipeRect)) {
+            if(y<bottomPipeRect.left) {
+                //the ellipse would have to intersect the left corner
+                if(pointInsideEllipse(bottomPipeRect.left, bottomPipeRect.top, x, y, horizontalRadius, radius))
+                    return true
+
+            } else if(bottomPipeRect.right<y) {
+                //the ellipse would have to intersect the right corner
+                if(pointInsideEllipse(bottomPipeRect.right, bottomPipeRect.top, x, y, horizontalRadius, radius))
+                    return true
+            }
+            else {
+                //the middle part of the ellipse is between the left and right side of the pipe
+                //it has to intersect
+                return true
+            }
+        }
+        return false
+
     }
     fun updateTarget() {
         //pitch is -1 if does not exist because of an error or low confidence level
